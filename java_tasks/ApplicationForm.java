fa984a1c99471d101d356714ee39e1d30628f747
Madhuri-131 <madhurichowdary0496@gmail.com>
2025-07-20 17:09:40 +0530

    Variables & Datatypes

---
+++ java_tasks/ApplicationForm.java
@@ -0,0 +1,84 @@
+public class ApplicationForm {
+    public static void main(String[] args)
+    {
+        String IsStudent = "1st Year";  // or Transferee or Returnee
+        String LastName = "Kannaur";
+        String FirstName = "Lokesh";
+        String MiddleName = "Rahul";
+        String CompleteAddress = "Bangalore, Karnataka";
+        String DateOfBirth = "18/April/1992";
+        String BirthPlace = "Bangalore";
+        String Citizenship = "Others"; // or Filipino
+        String CivilStatus = "Married"; // or Single
+        char Sex = 'M'; // or Female or Others
+        long ContactNum = 876543210;
+        String Religion = "Hindu";
+        String NameOfSpouse = "Athiya";
+        long SpouseContactNum = 345678543;
+        String FatherName = "K.N.Lokesh";
+        String FatherOccupation = "Professor";
+        long FatherContactNum = 788754543;
+        String MotherName = "Rajeshwari";
+        String MotherOccupation = "Professor";
+        long MotherContactNum = 948754543;
+        String GaurdianName = "Not Applicable";
+        short GaurdianContactNum = 0;
+        String GaurdianAddress = "Not Applicable";
+        String SeniorHighScl = "St. Aloysius College";
+        String SHSStrand = "PUC";
+        short YearGraduated = 2018;
+        byte GenAvg = 95;
+        String LastSclAttended = "NITK";
+        String Course = "MPC";
+        short LastSclYear = 2016;
+        boolean Are_u_an_ALSPasser_No = true;  // or false
+        short YearTaken = 2016;
+        byte Average = 85;
+        boolean Is_ur_family_4PsBeneficiary_No = false; //or true
+        String ID_4Ps = "Not Applicable";
+        boolean Any_disability_No = false; // or true
+        String TypeOfDisability = "Not applicable";
+
+
+        System.out.println(IsStudent);
+        System.out.println(LastName);
+        System.out.println(FirstName);
+        System.out.println(MiddleName);
+        System.out.println(CompleteAddress);
+        System.out.println(DateOfBirth);
+        System.out.println(BirthPlace);
+        System.out.println(Citizenship);
+        System.out.println(CivilStatus);
+        System.out.println(Sex);
+        System.out.println(ContactNum);
+        System.out.println(Religion);
+        System.out.println(NameOfSpouse);
+        System.out.println(SpouseContactNum);
+        System.out.println(FatherName);
+        System.out.println(FatherOccupation);
+        System.out.println(FatherContactNum);
+        System.out.println(MotherName);
+        System.out.println(MotherOccupation);
+        System.out.println(MotherContactNum);
+        System.out.println(GaurdianName);
+        System.out.println(GaurdianContactNum);
+        System.out.println(GaurdianAddress);
+        System.out.println(SeniorHighScl);
+        System.out.println(SHSStrand);
+        System.out.println(YearGraduated);
+        System.out.println(GenAvg);
+        System.out.println(LastSclAttended);
+        System.out.println(Course);
+        System.out.println(LastSclYear);
+        System.out.println(Are_u_an_ALSPasser_No);
+        System.out.println(YearTaken);
+        System.out.println(Average);
+        System.out.println(Is_ur_family_4PsBeneficiary_No);
+        System.out.println(ID_4Ps);
+        System.out.println(Any_disability_No);
+        System.out.println(TypeOfDisability);
+
+    }
+}
+    
+
